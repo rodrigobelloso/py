db5ef67c8c897c7f23d802f072b36549969340dc
rrodingo <114697724+rrodingo@users.noreply.github.com>
2023-10-22 18:06:36 +0200

    Ejercicio 2

--- Sesión 5/Ejercicio 2.py
+++ Sesión 5/Ejercicio 2.py
@@ -1,20 +1,13 @@
 n = int(input("Escribe un número máximo de valores: "))
 
-lista = []
-
-for i in range(n):
-    valor = int(input(f"Valor[{i+1}]= "))
-    lista.append(valor)
-
 sumaPositivos = 0
 contadorPositivos = 0
 
-for valor in lista:
+for i in range(n):
+    valor = int(input(f"Ingrese el valor {i + 1}: "))
     if valor > 0:
         sumaPositivos += valor
         contadorPositivos += 1
-
-print(f"La lista de valores es: {lista}.")
 
 print(f"Se escribieron {contadorPositivos} valores positivos.")
 print(f"La suma es {sumaPositivos}.")
