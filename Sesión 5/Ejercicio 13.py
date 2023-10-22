8c065b1245fe61071ef9aa6c68f6e123ca6ee47a
rrodingo <114697724+rrodingo@users.noreply.github.com>
2023-10-22 17:55:18 +0200

    ejercicios 13 y 14 actualizados

--- Sesión 5/Ejercicio 13.py
+++ Sesión 5/Ejercicio 13.py
@@ -1,21 +1,15 @@
-# Solicita al usuario ingresar dos números enteros
-num1 = int(input("Ingrese el primer número entero: "))
-num2 = int(input("Ingrese el segundo número entero: "))
+num1 = int(input("Escribe el primer número: "))
+num2 = int(input("Escribe el segundo número: "))
 
-# Calcula el valor absoluto de ambos números
-abs_num1 = abs(num1)
-abs_num2 = abs(num2)
+absolNum1 = abs(num1)
+absolNum2 = abs(num2)
 
-# Inicializa el resultado en 0
 resultado = 0
 
-# Realiza el producto de los valores absolutos mediante sumas sucesivas
-for _ in range(abs_num2):
-    resultado += abs_num1
+for i in range(absolNum2):
+    resultado += absolNum1
 
-# Determina el signo del resultado
 if (num1 < 0 and num2 > 0) or (num1 > 0 and num2 < 0):
     resultado = -resultado
 
-# Muestra el resultado
-print(f"El producto de {num1} y {num2} es: {resultado}")
+print("El producto de {} y {} es: {}".format(num1, num2, resultado))
